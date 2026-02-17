a4b21416a55eb59175501447af3a83b9b05e109e
Sai Krishna Podduturi <skrx7392@gmail.com>
2026-02-16 21:06:01 -0800

    test: add infrastructure test project and include in solution

---
+++ tests/Aarogya.Infrastructure.Tests/InfrastructureSmokeTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Aarogya.Infrastructure.Tests;
+
+public class InfrastructureSmokeTests
+{
+    [Fact]
+    public void SmokeTest_ShouldPass()
+    {
+        Assert.True(true);
+    }
+}
